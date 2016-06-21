827be9e6f9b6780a4f80bd2945f0957766584eb0
UI UX 2016 <UI UX 2016>
2016-06-21 15:11:14 +0100

    update rest apis and mocked json responses

--- server/server.js
+++ server/server.js
@@ -4,63 +4,51 @@ var fs = require("fs");
 var app = express();
 app.use(bodyParser.json());
 
-app.get('/locations', function(request, response) {
-    fs.readFile(__dirname + "/data/locations.json", 'utf8', function(err, data) {
-        console.log("Locations:", data);
+// GET a list of locations
+app.get('/locations', function (request, response) {
+    fs.readFile(__dirname + "/data/locations.json", 'utf8', function (err, data) {
         response.end(data);
     });
 });
 
-app.get('/locations/:id', function(request, response) {
+// GET a list of restaurants in a particular location
+app.get('/locations/:id', function (request, response) {
+   fs.readFile(__dirname + "/data/restaurants.json", 'utf8', function (err, data) {
+        var locationId = request.params.id;
+        data = JSON.parse(data);
+
+        var suitableRestaurants = [];
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].locationId == locationId) {
+                suitableRestaurants.push(data[i]);
+            }
+        }
+        response.end(JSON.stringify(suitableRestaurants));
+    });
+});
+
+// GET a menu for a particular restaurant
+app.get('/restaurants/:id', function (request, response) {
     if (request.params.id == 1) {
-        fs.readFile(__dirname + "/data/restaurants.json", 'utf8', function(err, data) {
-            console.log("Locations:", data);
-            response.end(data);
+        fs.readFile(__dirname + "/data/littleKitchenMenu.json", 'utf8', function (err, data) {
+            data = JSON.parse(data).RestaurantMenuCategories;
+            response.end(JSON.stringify(data));
         });
     } else {
-        response.status(404).send('Not found');
+        res.status(404).send('Not found');
     }
 });
 
-app.get('/restaurants/:id', function(request, response) {
-    fs.readFile(__dirname + "/data/restaurants.json", 'utf8', function(err, data) {
-        console.log("Restaurants:", data);
-        var restaurants = [];
-        data = JSON.parse(data);
-        
-        for (var d = 0, len = data.length; d < len; d += 1) {
-            console.log(data[d].locationId + " " + request.params.id);
-            if (data[d].locationId == request.params.id) {
-                restaurants.push(data[d]);
-            }
-        }
-        console.log(JSON.stringify(restaurants));
-        if(restaurants.length > 0) {
-            response.end(JSON.stringify(restaurants));
-        }
-        else{
-            response.status(404)
-                .send('Not found');
-        }
-    });
-});
-
-/*app.get('/order', function(request, response) {
-    fs.readFile(__dirname + "/data/order/json", 'utf8', function(err, data) {
-        console.log("Order", data);
-        response.end(data);
-    });
-});*/
-
-app.post('/orders', function(request, response) {
+// POST an order
+app.post('/orders', function (request, response) {
     var newOrder = request.body;
-    fs.readFile(__dirname + "/data/orders.json", 'utf8', function(err, data) {
+    fs.readFile(__dirname + "/data/orders.json", 'utf8', function (err, data) {
         data = JSON.parse(data);
         data.push(newOrder);
         response.end(JSON.stringify(data));
 
-        fs.writeFile(__dirname + "/data/orders.json", JSON.stringify(data), function(err) {
-            if(err) {
+        fs.writeFile(__dirname + "/data/orders.json", JSON.stringify(data), function (err) {
+            if (err) {
                 return console.error(err);
             }
         });
@@ -69,6 +57,6 @@ app.post('/orders', function(request, response) {
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(8081, function() {
+app.listen(8081, function () {
     console.log("Express listening on port 8081...");
 });
