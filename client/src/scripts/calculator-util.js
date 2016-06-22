ce87b46c89aef0e344665041506e8c44afcb3932
UI UX 2016 <UI UX 2016>
2016-06-22 15:53:28 +0100

    tidy up calculator

--- client/src/scripts/calculator-util.js
+++ client/src/scripts/calculator-util.js
@@ -1,16 +1,13 @@
-var Calculator = function() {
+var Calculator = function () {
 
     function calculateTotal(cartItems) {
         var totalCost = 0.00;
 
-        for (var i=0; i < cartItems.length; i++) {
+        for (var i = 0; i < cartItems.length; i++) {
             var itemCost = parseFloat(cartItems[i].price) * parseInt(cartItems[i].quantity);
-
-            for (var j=0; j < cartItems[i].variations.length; j++) {
+            for (var j = 0; j < cartItems[i].variations.length; j++) {
                 itemCost = itemCost + parseFloat(cartItems[i].variations[j].price);
-                console.log('itemCost with variation is ' + itemCost);
             }
-
             totalCost += itemCost;
         }
         return totalCost.toFixed(2);
