1a90c124b638232206f90c2457f5a9d5e8c0f88e
UI UX 2016 <UI UX 2016>
2016-06-22 09:48:20 +0100

    updates to gulpfile to make some tasks synchronous

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,42 @@
-var gulp = require('gulp'), less = require('gulp-less'),
+var gulp = require('gulp'),
+    less = require('gulp-less'),
     rename = require('gulp-rename'),
     gp_concat = require('gulp-concat'),
     gp_uglify = require('gulp-uglify'),
     del = require('del'),
-    jasmine = require('gulp-jasmine');
+    jasmine = require('gulp-jasmine'),
+    runSequence = require('run-sequence');
 
-gulp.task('default', [ 'build', 'test']);
+gulp.task('default', function() {
+    runSequence('clean', 'build', 'test');
+});
 
 gulp.task('clean', function () {
     return del(['dist']);
-    
 });
 
 gulp.task('build:styles', function () {
-
-
     gulp.src('client/src/styles/main.less') // path to your file
         .pipe(less().on('error', function (err) {
             console.log(err);
         }))
         .pipe(gulp.dest('dist/server/public/styles/'));
-
 });
 
 gulp.task('build:scripts', function () {
-
-
-    gulp.src([ 'client/src/scripts/restaurants.js', 'client/src/scripts/main.js'])
+    gulp.src('client/src/scripts/**/*.js')
         .pipe(gp_concat('main.js'))
         .pipe(gp_uglify())
         .pipe(gulp.dest('dist/server/public/scripts/'));
-
-
 });
 
 gulp.task('build:html', function () {
     gulp.src('client/src/html/index.html')
-    // Perform minification tasks, etc here
         .pipe(gulp.dest('dist/server/public/'));
 });
 
 gulp.task('build:img', function () {
     gulp.src('client/src/img/**/*')
-    // Perform minification tasks, etc here
         .pipe(gulp.dest('dist/server/public/img/'));
 });
 
@@ -51,8 +45,14 @@ gulp.task('build:server', function () {
         .pipe(gulp.dest('dist/server'));
 });
 
-gulp.task('build', ['build:styles', 'build:scripts', 'build:html', 'build:server', 'build:img']);
+gulp.task('build', [
+    'build:styles',
+    'build:scripts',
+    'build:html',
+    'build:server',
+    'build:img']);
 
 gulp.task('test', function () {
-    gulp.src(['client/test/**/*.js','server/test/**/*.js' ]).pipe(jasmine());
+    gulp.src(['client/test/**/*.js', 'server/test/**/*.js'])
+        .pipe(jasmine());
 });
