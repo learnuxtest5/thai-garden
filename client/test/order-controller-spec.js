caad3dadfe440933f61b7d487284ffd5c0e0b4da
UI UX 2016 <UI UX 2016>
2016-06-23 10:36:33 +0100

    add method to controller to allow user to use a coupon for a discount

--- client/test/order-controller-spec.js
+++ client/test/order-controller-spec.js
@@ -1,6 +1,7 @@
 describe('order-controller-spec', function() {
 
     it('should check the public api', function() {
+        expect(typeof OrderController.addCoupon).toBe('function');
         expect(typeof OrderController.addItemToCart).toBe('function');
         expect(typeof OrderController.removeItemFromCart).toBe('function');
         expect(typeof OrderController.sendOrder).toBe('function');
