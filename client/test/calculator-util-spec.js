e828f405a615abe63a0b4dbda8f4da4f5d1dd21c
UI UX 2016 <UI UX 2016>
2016-06-23 11:01:00 +0100

    add unit test for calculator util

--- client/test/calculator-util-spec.js
+++ client/test/calculator-util-spec.js
@@ -1,6 +1,28 @@
 describe('calculator-util-spec', function() {
 
-    xit('should calculate the total price of all items in the cart', function() {
+    it ('should check the public api', function() {
+        expect(typeof Calculator.calculateTotal).toBe('function');
+    });
 
+    it('should calculate the total price of all items in the cart', function() {
+
+        var cartItem = {
+            restaurantId: 1,
+            categoryId: 2,
+            itemId: 3,
+            price: 11.00,
+            quantity: 3,
+            variations: [
+                {
+                    id: 4,
+                    price: 6.50
+                }
+            ]
+        };
+
+        var cartItems = [];
+        cartItems.push(cartItem);
+
+        expect(Calculator.calculateTotal(cartItems)).toBe('39.50');
     });
 });
