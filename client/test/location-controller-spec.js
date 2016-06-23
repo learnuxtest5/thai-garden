511fb66031723ef30c38b8a0db511ae880e40a42
UI UX 2016 <UI UX 2016>
2016-06-23 10:00:16 +0100

    fix build issue

--- client/test/location-controller-spec.js
+++ client/test/location-controller-spec.js
@@ -2,5 +2,5 @@ describe('location-controller-spec', function() {
 
     /*it('should check the public api', function() {
         expect(typeof LocationController.getLocations).toBe('function');
-    });*
+    });*/
 });
