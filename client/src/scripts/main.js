000754aeb05d050c453470a9b85273ed049c6365
UI UX 2016 <UI UX 2016>
2016-06-23 10:07:59 +0100

    latest update with templating in controller

--- client/src/scripts/main.js
+++ client/src/scripts/main.js
@@ -1,22 +1,16 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    LocationController.getLocations().then(function(response) {
-        console.log("Locations", response);
-        console.log(loadPage("templates/locations.html"));
-        buildTemplate(null, loadPage("templates/locations.html"), "content-container");
-    });
+    //trigger hash change to start the application
+    $(window).trigger('hashchange');
+    
 
-    RestaurantController.getRestaurants(1).then(function(response) {
-        console.log("Restaurants", response);
-    });
 
-    MenuController.getMenu(1).then(function(response) {
-        console.log("Menu", response);
-    });
+
 
     OrderController.addItemToOrder(1, 199, 958, 13.95, 1, []);
     OrderController.addItemToOrder(2, 199, 958, 13.95, 1, []);
     sessionStorage.getItem('orderItems');
+
 
     OrderController.sendOrder().then(function(response) {
         console.log("Order", response);
@@ -24,26 +18,6 @@ $(document).ready(function() {
     });
 });
 
-function loadPage(href)
-{
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", href, false);
-    xmlhttp.send();
-    return xmlhttp.responseText;
-}
-function buildTemplate(model, template, documentContainerId){
-    document.getElementById(documentContainerId).innerHTML = template;
-    LocationController.getLocations();
-    outputTemplate("restaurant");
 
-};
-function outputTemplate(data){
-    console.log(data);
-
-    var nodesArray = Array.prototype.slice.call(document.querySelectorAll('[data-' + data +  ']'));
-    for(var i=0; i<nodesArray.length; i++){
-        nodesArray[i].innerHTML = "Testing " + i;
-    }
-}
 
 
--- client/src/scripts/menu-controller.js
+++ client/src/scripts/menu-controller.js
@@ -6,7 +6,35 @@ var MenuController = function () {
         });
     }
 
+    function getView(model){
+        var htmlView = "";
+        for(var i=0; i<model.length; i++){
+            htmlView += createSection(model[i]);
+        }
+        
+        return htmlView;
+    }
+
+    function createSection(section){
+        var htmlSection = "<div><h3>" + section.Title +  "</h3>";
+        for(var i=0; i<section.length; i++){
+            htmlSection += createMenuItems(section.Items);
+        }
+        htmlSection += "</div>";
+        return htmlSection;
+    }
+
+    function createMenuItems(menuItems){
+        var htmlMenuItems = "<div>"
+        for(var i=0; i<menuItems.length; i++){
+            htmlMenuItems += "<div><h4>" + menuItems[i].Title + "</h4><span>€" + menuItems[i].Price + "</span></div>"
+        }
+        htmlMenuItems = "</div>"
+        return htmlMenuItems;
+    }
+
     return {
-        getMenu: getMenu
+        getMenu: getMenu,
+        getView: getView
     };
 }();
