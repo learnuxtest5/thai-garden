42ea761769842ed60e8c36a491b73d3c35605812
UI UX 2016 <UI UX 2016>
2016-06-23 10:22:51 +0100

    write SpecRunner instead of running test during build

--- client/test/location-controller-spec.js
+++ client/test/location-controller-spec.js
@@ -1,6 +1,6 @@
 describe('location-controller-spec', function() {
 
-    /*it('should check the public api', function() {
+    it('should check the public api', function() {
         expect(typeof LocationController.getLocations).toBe('function');
-    });*/
+    });
 });
--- client/test/order-controller-spec.js
+++ client/test/order-controller-spec.js
@@ -1,8 +1,8 @@
 describe('order-controller-spec', function() {
 
-    /*it('should check the public api', function() {
+    it('should check the public api', function() {
         expect(typeof OrderController.addItemToCart).toBe('function');
         expect(typeof OrderController.removeItemFromCart).toBe('function');
         expect(typeof OrderController.sendOrder).toBe('function');
-    });*/
+    });
 });
