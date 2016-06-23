50924e177662b7f60008a8a981f2e0d754411b51
UI UX 2016 <UI UX 2016>
2016-06-23 15:39:15 +0100

    add basic structure for cart

--- client/src/scripts/main.js
+++ client/src/scripts/main.js
@@ -1,27 +1,58 @@
 $(document).ready(function () {
 
-    //trigger hash change to start the application
+    // trigger hash change to start the application
     $(window).trigger('hashchange');
-    
-    /*OrderController.addItemToCart(1, 199, 123, 13.95, 2, []);
-    sessionStorage.getItem('cart.items');
-    sessionStorage.getItem('cart.totalPrice');
 
-    OrderController.addItemToCart(2, 199, 234, 10, 5, [{id: 976, price: 3.50}, {id: 975, price: 5.75}]);
-    sessionStorage.getItem('cart.items');
-    sessionStorage.getItem('cart.totalPrice');
+    $('[class^=cart-logo-]').click(function () {
+        console.log('show shopping cart');
 
-    OrderController.removeItemFromCart(234);
-    sessionStorage.getItem('cart.items');
-    sessionStorage.getItem('cart.totalPrice');
-    
-    OrderController.sendOrder().then(function(response) {
-        console.log("Order", response);
-        console.log("Order Number", response.orderNumber);
-        console.log("Order Price", response.totalPrice);
+        // show the cart using class applied by jQuery
     });
-    sessionStorage.getItem('cart.items');
-    sessionStorage.getItem('cart.totalPrice');*/
+
+    $('.hide-cart').click(function () {
+        console.log('hide shopping cart');
+
+        // hide the cart using class applied by jQuery
+    });
+
+    $('.add-coupon').click(function () {
+        console.log('add coupon');
+
+        var discount = $('.coupon-value').text();
+        discount = parseFloat(discount) / 100.0;
+
+        OrderController.addCoupon(discount);
+    });
+
+    $('.proceed-checkout').click(function () {
+        console.log('proceed to checkout');
+
+        // hide the proceed button using jQuery
+
+        // show the payment details using jQuery
+    });
+
+    $('.place-order').click(function () {
+        console.log('place order');
+
+        //TODO: validate inputs before they can proceed
+
+        var orderType = $('.order-type input:checked').text();
+        var deliveryAddress = $('.delivery-address input').text();
+        var collectionTime = $('.collection-time option:selected').text()
+        var paymentType = $('.payment-type input[type="radio"]:checked').text();
+        var cardNumber = $('.payment-type input#card-number').text();
+        var cardType = $('.payment-type input#card-type').text();
+        var name = $('.customer-name input').text();
+        var phoneNumber = $('.customer-phone input').text();
+
+        OrderController.sendOrder(
+            orderType, deliveryAddress, collectionTime,
+            paymentType, cardNumber, cardType,
+            name, phoneNumber).then(function (response) {
+            // show the confirmation when the order is placed
+        });
+    });
 });
 
 
