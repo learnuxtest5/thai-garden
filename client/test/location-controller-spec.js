5fb4bce6100115990a7a42b83b15648d94fb0e3c
UI UX 2016 <UI UX 2016>
2016-06-23 09:54:26 +0100

    add basic specs

--- client/test/location-controller-spec.js
+++ client/test/location-controller-spec.js
@@ -1,3 +1,6 @@
 describe('location-controller-spec', function() {
 
+    it('should check the public api', function() {
+        expect(typeof LocationController.getLocations).toBe('function');
+    });
 });
